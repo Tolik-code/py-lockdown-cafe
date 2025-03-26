683deab19893c5d594842989472d22f6329d19a4
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-03-26 18:46:19 +0200

    moved visitor name to var

--- app/cafe/cafe.py
+++ app/cafe/cafe.py
@@ -11,15 +11,16 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> None | str:
+        visitor_name = visitor["name"]
         if "vaccine" not in visitor:
-            raise NotVaccinatedError(f"{visitor["name"]} has no vaccine")
+            raise NotVaccinatedError(f"{visitor_name} has no vaccine")
 
         if visitor["vaccine"]["expiration_date"] < datetime.now().date():
             raise OutdatedVaccineError(
-                f"{visitor["name"]}'s vaccine certificate has expired"
+                f"{visitor_name}'s vaccine certificate has expired"
             )
 
         if not visitor["wearing_a_mask"]:
-            raise NotWearingMaskError(f"{visitor["name"]} should wear a mask")
+            raise NotWearingMaskError(f"{visitor_name} should wear a mask")
 
         return f"Welcome to {self.name}"
